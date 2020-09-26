fc0c37f664e6b6bafb1ab4344e71c87486042325
Joy Cho <jcho@sqasquared.com>
2020-09-25 17:51:59 -0700

    New branch, edited app.py

--- app.py
+++ app.py
@@ -17,10 +17,21 @@
 
 # int() float() bool() str()
 
-firstNum = input("Enter your first number: ")
-secondNum = input("Enter your second number: ")
+# firstNum = input("Enter your first number: ")
+# secondNum = input("Enter your second number: ")
 
-sum = float(firstNum) + float(secondNum)
+# sum = float(firstNum) + float(secondNum)
 
-print("Sum: " + str(sum))
+# print("Sum: " + str(sum))
+
+# firstNum = float(input("Enter your first number: "))
+# secondNum = float(input("Enter your second number: "))
+#
+# sum = str(firstNum + secondNum)
+#
+# print("Sum: " + sum)
+
+# course = 'python for beginners'
+# print(course.replace('for', '4'))
+# print(course)
 
