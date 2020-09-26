403823b23e494da45321c127be529ef9f5eebb38
Joy Cho <jcho@sqasquared.com>
2020-09-25 17:23:50 -0700

    New python file, folliwng "Programming with Mosh"'s tutorial for beginners [2020]

---
+++ app.py
@@ -0,0 +1,26 @@
+# firstName = "John"
+# lastName = "Smith"
+# newPatient = True
+
+# name = input("What is your name? ")
+# print("Hello " + name)
+
+# oopsies! error int - string
+# birthYear = input("Enter your birth year: ")
+# age = 2020 - birthYear
+# print(age)
+
+# better now
+# birthYear = input("Enter your birth year: ")
+# age = 2020 - int(birthYear)
+# print(age)
+
+# int() float() bool() str()
+
+firstNum = input("Enter your first number: ")
+secondNum = input("Enter your second number: ")
+
+sum = float(firstNum) + float(secondNum)
+
+print("Sum: " + str(sum))
+
