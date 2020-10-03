8d2d3e301060ff5e560eb73c947309502e6e8a67
Joy Cho <jcho@sqasquared.com>
2020-10-02 17:52:31 -0700

    more py practice
    python practice following CD Dojo's instructions/python tutorial videos

--- app.py
+++ app.py
@@ -58,5 +58,54 @@ class Robot:
 r1 = Robot("tom", "red", 30)
 r2 = Robot("Jerry", "blue", 40)
 
-r1.introduceSelf()
-r2.introduceSelf()
+# r1.introduceSelf()
+# r2.introduceSelf()
+
+class Person:
+    def __init__(self, name, personality, isSitting):
+        self.name = name
+        self.personality = personality
+        self.isSitting = isSitting
+
+    def sitDown(self):
+        self.is_sitting = True
+
+    def standUp(self):
+        self.is_sitting = False
+
+p1 = Person("Alice", "aggressive", False)
+p2 = Person("Becky", "talkative", True)
+
+#p1 owns r2
+p1.robotOwned = r2
+
+#p2 owns r1
+p2.robotOwned = r1
+
+# p1.robotOwned.introduceSelf()
+
+# print(type(True))
+
+def c_greater_than_d_plus_e(c, d, e):
+    return c > (d + e)
+
+# print(c_greater_than_d_plus_e(10, 2, 20))
+
+a = [1, 3, 5, 7, 9, 11]
+#.append()
+b = []
+b.append(2)
+b.clear()
+# print(b)
+
+c = []
+for x in a:
+    c.append(x * 2)
+
+print(c)
+
+# same thing as above
+d = [x * 2 for x in a]
+print(d)
+
+e1 = []
