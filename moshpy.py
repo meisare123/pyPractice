14a52657f35e67ddc9320bdcdc3e3b5a97386a80
Joy Cho <jcho@sqasquared.com>
2020-10-12 17:54:42 -0700

    Continuing in doing mosh video tutorial

--- moshpy.py
+++ moshpy.py
@@ -1,3 +1,62 @@
-birthYear = int(input("Enter birth year: "))
-age = 2020 - birthYear
-print(age)
+# birthYear = int(input("Enter birth year: "))
+# age = 2020 - birthYear
+# print(age)
+
+
+
+# weight = int(input("Weight: "))
+# measurement = input("(K)g or (L)bs: ")
+# newWeigth = 0
+#
+# if (measurement.upper() == "K"):
+#     newWeigth = str(weight * 2.205)
+#     print("Weight in Lb: " + newWeigth)
+# elif (measurement.upper() == "L"):
+#     newWeigth = str(weight * 2.205)
+#     print("Weight in Kg: " + newWeigth)
+# else:
+#     print("Error! Invalid input! Must choose 'K' or 'L'")
+
+
+
+# i = 0
+# while i < 10:
+#     print(i * "*")
+#     i += 1
+
+
+
+# names = ["John", "Bob", "Mosh", "Sam", "Mary"]
+# names[0] = "Jon"
+# print(names[0:3]) # names[-2]     names[0:3]
+
+
+# print("a".upper)
+# print("a".upper())
+
+
+
+# numbers = [1, 2, 3, 4, 5]
+# numbers.append(6)
+# print(numbers)
+# numbers.insert(1, 12)
+# print(numbers)
+# numbers.insert(0, -1)
+# print(numbers)
+# numbers.remove(3)
+# print(numbers)
+# numbers.clear()
+# print(numbers)
+# print(11 in numbers)
+# print(len(numbers))
+
+
+
+numbers = [1, 2, 3, 4, 5]
+for i in numbers:
+    print(i)
+# do the same thing with a while
+# i = 0
+# while (i < len(numbers)):
+#     print(i+1)
+#     i += 1
